a72f4877f22bdb7e37bed0e61e1c262ede6b2719
BENAKMOUME Yacine <soudoufou@hotmail.fr>
2020-11-23 12:27:39 +0100

    Ajout des documents

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,11 +1,18 @@
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
-  './',
-  './index.html',
-  './app.js',
-  './style.js',
-  './pwamanifest.webmanifest',
-  './icons/favicon.png',
+"use strict";
+
+/** 
+    Source: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+*/
+
+// variable definitions 
+var CACHE_NAME = 'ginkobusPWA-v1';
+
+var contentToCache = [
+  './index.html',    
+  './pwamanifest.webmanifest',    
+  './style.css', 
+  './app.js', 
+  './icons/favicon.ico',
   './icons/icon-32.png',
   './icons/icon-64.png',
   './icons/icon-96.png',
@@ -14,42 +21,49 @@ var appShellFiles = [
   './icons/icon-180.png',
   './icons/icon-192.png',
   './icons/icon-256.png',
-  './icons/maskable_icon.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './icons/maskable_icon.png'
 ];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil(caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(contentToCache);
-    })
-  );
+// service worker installation
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log('[Service Worker] Caching application content & data');
+        return cache.addAll(contentToCache);
+    }));
 });
+
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+   
+    // Stratégie initiale : cache ou network avec mise en cache 
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || 
+                fetch(e.request).then((response) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        console.log('[Service Worker] Caching new resource: '+e.request.url);
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                });
+        })
+    );
 });
+    
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-        if(key !== cacheName) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+    e.waitUntil(
+        // cleaning previous caches
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if(CACHE_NAME.indexOf(key) === -1) {
+                    console.log("[Service Worker] Cleaning old cache");
+                    return caches.delete(key);
+                }
+          }));
+        })
+    );
 });
